8c112d0194bd495564c67625a5755ff2b758ebc7
bayllech <hpuqibei@63.com>
2018-03-28 15:28:07 +0800

    springboot建立

---
+++ corpid/src/main/java/cn/bayllech/corpid/CorpidApplication.java
@@ -0,0 +1,12 @@
+package cn.bayllech.corpid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpidApplication.class, args);
+	}
+}
