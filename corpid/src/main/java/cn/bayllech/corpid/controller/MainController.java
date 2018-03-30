6ca008dfa425306c3f1518f1ac838f01ffee6b0d
bayllech <hpuqibei@163.com>
2018-03-30 14:10:06 +0800

    向已关注用户发送消息

--- corpid/src/main/java/cn/bayllech/corpid/controller/MainController.java
+++ corpid/src/main/java/cn/bayllech/corpid/controller/MainController.java
@@ -2,6 +2,7 @@ package cn.bayllech.corpid.controller;
 
 import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.cp.api.WxCpService;
+import me.chanjar.weixin.cp.bean.WxCpMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,10 @@ public class MainController {
     @RequestMapping
     public String index() throws WxErrorException {
         System.out.println(service.getAccessToken());
+
+        String userId = "bayllech";
+        WxCpMessage message = WxCpMessage.TEXT().agentId(service.getWxCpConfigStorage().getAgentId()).toUser(userId).content("Hello World").build();
+        service.messageSend(message);
 
         logger.debug("Access_token: {}",service.getAccessToken());
         return service.getAccessToken();
