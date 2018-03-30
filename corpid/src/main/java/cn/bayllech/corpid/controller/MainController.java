a30b3d678574c30e0ca285782f16dac0729771c0
bayllech <hpuqibei@163.com>
2018-03-30 13:58:43 +0800

    获取微信access_token

--- corpid/src/main/java/cn/bayllech/corpid/controller/MainController.java
+++ corpid/src/main/java/cn/bayllech/corpid/controller/MainController.java
@@ -1,21 +1,32 @@
 package cn.bayllech.corpid.controller;
 
-import org.springframework.stereotype.Controller;
+import me.chanjar.weixin.common.exception.WxErrorException;
+import me.chanjar.weixin.cp.api.WxCpService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Administrator
  * @date 2018/3/29
  */
 
-@Controller
+@RestController
 @RequestMapping
 public class MainController {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    WxCpService service;
+
     @RequestMapping
-    @ResponseBody
-    public String index() {
-        return "Hello";
+    public String index() throws WxErrorException {
+        System.out.println(service.getAccessToken());
+
+        logger.debug("Access_token: {}",service.getAccessToken());
+        return service.getAccessToken();
     }
 }
